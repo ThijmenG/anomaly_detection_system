37615c553b4ea504047744cf6f6aea52c315093f
Nirmalkumar <balanss77@gmail.com>
2024-05-28 13:19:41 +0200

    Push model update with thresholds

--- UI_files/model_handler.py
+++ UI_files/model_handler.py
@@ -3,6 +3,7 @@ import numpy as np
 from datetime import datetime, timedelta
 from Model.data_preprocessor import read_initial_data, outlier_treatment, clearWeekends, scaled_predict, timelagged, scaled_train
 from Model.results import predict_results, anomaly_flag
+from Model.model_builder import lstm_model, train_model, plot_model
 
 def run_model(file_path, new_data, pressure_threshold=-0.3):
     print(f"Running model with file: {file_path}")
@@ -13,6 +14,8 @@ def run_model(file_path, new_data, pressure_threshold=-0.3):
         preprocess_data = clearWeekends(new_data)
         print('Cleared weekends')
 
+        preprocess_data = read_initial_data(preprocess_data)
+
         # Remove recipe number
         if '18Recept - Receptnummer oven 2' in preprocess_data.columns:
             preprocess_data = preprocess_data.drop(columns=['18Recept - Receptnummer oven 2'])
@@ -20,13 +23,10 @@ def run_model(file_path, new_data, pressure_threshold=-0.3):
         else:
             print('Recipe number column not found')
 
-        # Make date the index
-        preprocess_data['Date'] = pd.to_datetime(preprocess_data['Date'], dayfirst=True)
-        preprocess_data = preprocess_data.set_index('Date')
         print('Set date as index')
         print(preprocess_data.head())
 
-        preprocess_data = scaled_predict(preprocess_data)
+        preprocess_data = scaled_predict(preprocess_data, -0.3)
         print('Scaled data')
 
         preprocess_data = timelagged(preprocess_data, n_past=6)  # Example value for n_past
@@ -35,7 +35,7 @@ def run_model(file_path, new_data, pressure_threshold=-0.3):
         print('Preprocessing done')
 
         # 2. Predicting with the model
-        predictions = predict_results(new_data, preprocess_data)
+        predictions = predict_results(new_data, preprocess_data, -0.3)
         print('Predictions done')
 
         anomaly_predictions = anomaly_flag(predictions)
@@ -66,14 +66,14 @@ def train_model(file_path, new_data, pressure_threshold = -0.3):
             print('Recipe number column not found')
 
         # Make date the index
-        preprocess_data['Date'] = pd.to_datetime(preprocess_data['Date'], dayfirst=True)
-        preprocess_data = preprocess_data.set_index('Date')
-        print('Set date as index')
+        preprocess_data = read_initial_data(preprocess_data)
+
+        print('Read initial data')
         print(preprocess_data.head())
 
-        preprocess_data = outlier_treatment(preprocess_data, pressure_threshold= pressure_threshold , moisture_lower= 0 , moisture_upper= 15 )
+        preprocess_data = outlier_treatment(preprocess_data, pressure_threshold= pressure_threshold , moisture_upper= 15 , moisture_lower= 0 )
 
-        preprocess_data = scaled_train(preprocess_data)
+        preprocess_data = scaled_train(preprocess_data, -0.3)
         print('Scaled data')
 
         preprocess_data = timelagged(preprocess_data, n_past=6)  # Example value for n_past
@@ -81,16 +81,13 @@ def train_model(file_path, new_data, pressure_threshold = -0.3):
 
         print('Preprocessing done')
 
-        # 2. Predicting with the model
-        predictions = predict_results(new_data, preprocess_data)
-        print('Predictions done')
+        model = lstm_model(preprocess_data)
 
-        anomaly_predictions = anomaly_flag(predictions)
-        print('Anomaly detection done')
-        
-        print(f'Anomaly list : {anomaly_predictions}')
+        history = train_model(preprocess_data, model, -0.3)
 
-        return new_data, anomaly_predictions  # returning data to be plotted
+        fig = plot_model(history)
+
+        return fig  # returning data to be plotted
 
     except Exception as e:
         print(f"An error occurred: {e}")
